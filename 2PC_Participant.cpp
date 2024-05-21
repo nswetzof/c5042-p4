02f8093068404ea62c2b85512a77bf5a9212857a
Nathan Swetzof <nswetzof@gmail.com>
2024-05-20 21:38:01 -0700

    Partially implement process method.

--- 2PC_Participant.cpp
+++ 2PC_Participant.cpp
@@ -24,3 +24,74 @@ Participant::Participant(const u_short port, const string acc_file_name, const s
 }
 
 Participant::~Participant() {}
+
+bool Participant::process(const string &incoming_stream_piece) {
+    // TODO: have to log
+    int first_space = incoming_stream_piece.find(' ');
+    vector<string> request = split(incoming_stream_piece);
+    string type = request.at(0);
+    string account;
+    double amount;
+
+    if (type == "VOTE-REQUEST") {
+        amount = atof(request.at(1).c_str());
+        account = request.at(2);
+
+        if (accounts.find(account) != accounts.end()) {
+            if (accounts[account].balance + amount >= 0 && !accounts[account].held) {
+                accounts[account].held = true;
+                respond("VOTE-COMMIT");
+                return true;
+            }
+        }
+
+        respond("VOTE-ABORT");
+        return false;
+    }
+
+    if (type == "GLOBAL-COMMIT") {
+        respond("ACK");
+        // TODO: execute transaction and eliminate hold
+        // TODO: need to determine protocol for this message type
+        /*
+        check if account exists
+            check if hold has been placed
+                execute transaction
+                respond that transaction was executed
+        else
+            failure message
+            remove hold
+        */
+        return true;
+    }
+
+    if (type == "GLOBAL-ABORT") {
+        respond("ACK");
+        // TODO: eliminate hold
+        return true;
+    }
+    // TODO
+    /* 
+    if deposit
+        if account exists
+            VOTE-COMMIT
+        else
+            VOTE-ABORT
+    if withdraw
+        if account exists and sufficient funds
+            VOTE-COMMIT
+        else
+            VOTE-ABORT
+    */
+}
+
+vector<string> split(const string &text, const char delimiter) {
+    vector<string> result;
+    int prev = 0;
+    int index = text.find(delimiter);
+    while (index != string::npos) {
+        result.push_back(text.substr(prev, index - prev));
+        prev = index;
+        index = text.find(delimiter, index);
+    }
+}
--- 2PC_Participant.h
+++ 2PC_Participant.h
@@ -2,6 +2,7 @@
 
 #include <fstream>
 #include <string>
+#include <vector>
 #include <unordered_map>
 #include "TCPServer.h"
 
@@ -17,14 +18,33 @@ public:
 protected:
     /**
      * Process transaction request
+     * @param incoming_stream_piece Message received from the coordinator.  Valid message types are as follows:
+     *  VOTE-REQUEST [amount] [account] - request a transaction from account specified of amount specified (positive or negative)
+     *      If the account exists and there are sufficient funds to execute the transaction, a hold will be placed
+     *      and a VOTE-COMMIT response will be sent to the coordinator
+     *  GLOBAL-COMMIT - execute the transaction
+     *  GLOBAL-ABORT - abort the transaction
+     * @return true if request is satisfied, false otherwise
     */
-    // bool process(const std::string &incoming_stream_piece) override;
-    // void respond(const std::string &response) override;
+    bool process(const std::string &incoming_stream_piece) override;
+    // void respond(const std::string &response) override; // TODO: Delete?  Don't see why this needs to be overridden
 private:
-    unordered_map<string, double> accounts;
+    struct account {
+        double balance;
+        bool held;
+    };
+    unordered_map<string, account> accounts;
     const string acc_file_name, log_file_name;
     // ifstream acc_file;
     // ofstream log_file;
 
-    
+    /**
+     * Splits a string into multiple strings defined by a delimiter
+     * @return List of strings after splitting the input string
+     * @param text String to split
+     * @param delimiter Character which delimits the separation
+    */
+    vector<string> split(const string &text, const char delimiter = ' ');
+    bool processDeposit(string acc_num, double balance);
+    bool processWithdrawal(string acc_num, double balance);
 };
