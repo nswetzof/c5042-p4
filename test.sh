27dfdb7d5503e47eab4f2c5f6bc477f38c49846a
Nathan Swetzof <nswetzof@gmail.com>
2024-05-26 15:24:28 -0700

    Add shell script to run client and server programs at once.

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
-./participant 10101 accounts1.txt log1.txt
-./participant 10102 accounts2.txt log2.txt
-./coordinator log.txt 
+make all
+cmd.exe /c start wsl.exe --cd "~/projects/c5042/p2" ./participant 10101 accounts1.txt log1.txt
+cmd.exe /c start wsl.exe --cd "~/projects/c5042/p2" ./participant 10102 accounts2.txt log2.txt
+sleep 1
+./coordinator log.txt 1.00 localhost 10101 8901-23.45 localhost 10102 2345-67.89
