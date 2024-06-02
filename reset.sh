e7cf41e411fe44e8a1625f2b467b4c16d6617a81
Nathan Swetzof <nswetzof@gmail.com>
2024-06-01 23:02:15 -0700

    Modify supporting files to account for a 3rd participant.

--- reset.sh
+++ reset.sh
@@ -1,10 +1,14 @@
 rm accounts1.txt
 rm accounts2.txt
+rm accounts3.txt
 rm log.txt
 rm log1.txt
 rm log2.txt
+rm log3.txt
 cp accounts1_backup.txt accounts1.txt
 cp accounts2_backup.txt accounts2.txt
+cp accounts3_backup.txt accounts3.txt
 touch log.txt
 touch log1.txt
 touch log2.txt
+touch log3.txt
